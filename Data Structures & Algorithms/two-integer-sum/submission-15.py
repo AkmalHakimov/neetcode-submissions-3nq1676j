6e748050cf9b647a3f783257972a1a07a757ffaf
Akmal <118215825+AkmalHakimov@users.noreply.github.com>
2026-06-23 14:08:11 +0500

    Add: two-integer-sum - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum/submission-15.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        mp = {}
+        for i in range(len(nums)):
+            third = target - nums[i]
+            if third in mp:
+                return [mp[third], i]
+            mp[nums[i]] = i
+     
